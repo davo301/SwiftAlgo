6ca9b90c75a90d4a2887af746ab389eac198e838
Davit Siradeghyan <davit.siradeghyan@disqo.com>
2019-09-20 17:27:24 +0400

    Added testcase for custom class

--- SwiftAlgo/SwiftAlgoTests/SwiftAlgoTests.swift
+++ SwiftAlgo/SwiftAlgoTests/SwiftAlgoTests.swift
@@ -36,7 +36,10 @@ class SwiftAlgoTests: XCTestCase {
     }
     
     private func setupCustomClassContainer() {
-        
+        customClassContainer.append(CustomClass(name: "John", score: 4.65))
+        customClassContainer.append(CustomClass(name: "David", score: 4))
+        customClassContainer.append(CustomClass(name: "Alex", score: 3.1))
+        customClassContainer.append(CustomClass(name: "Karine", score: 2.2))
     }
     
     private func setupTestStructBasicTypes() {
@@ -55,6 +58,12 @@ class SwiftAlgoTests: XCTestCase {
         
         count = SwiftAlgo.count(value: "aaa", inContainer: stringContainer)
         XCTAssertEqual(count, 1)
+        
+        count = SwiftAlgo.count(value: CustomClass(name: "Alex", score: 5), inContainer: customClassContainer)
+        XCTAssertEqual(count, 0)
+        
+        count = SwiftAlgo.count(value: CustomClass(name: "Alex", score: 3.1), inContainer: customClassContainer)
+        XCTAssertEqual(count, 1)
     }
     
     func test_countWithPredicate() {
@@ -63,6 +72,9 @@ class SwiftAlgoTests: XCTestCase {
         
         count = SwiftAlgo.count(inContainer: stringContainer, withPredicate: { $0.contains("aaa") })
         XCTAssertEqual(count, 2)
+        
+        count = SwiftAlgo.count(inContainer: customClassContainer, withPredicate: { $0.score > 3 })
+        XCTAssertEqual(count, 3)
     }
     
 }
