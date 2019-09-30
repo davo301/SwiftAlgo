f71b35a867897441b115a809e1631efef06422c1
Davit Siradeghyan <davit.siradeghyan@disqo.com>
2019-09-30 22:40:24 +0400

    Modified counts test

--- SwiftAlgo/SwiftAlgoTests/Algorithms/SwiftAlgoTestsCount.swift
+++ SwiftAlgo/SwiftAlgoTests/Algorithms/SwiftAlgoTestsCount.swift
@@ -10,89 +10,44 @@ import XCTest
 
 class SwiftAlgoTestsCount: XCTestCase {
     
-    private var intContainer = [Int]()
-    private var stringContainer = [String]()
-    private var customClassContainer = [CustomClass]()
-    private var testStructBasicTypes = [TestStructBasicTypes]()
-    private var testStructCustomTypes = [TestStructCustomTypes]()
-    
-    override func setUp() {
-        super.setUp()
-        setupIntContainer()
-        setupStringContainer()
-        setupCustomClassContainer()
-        setupTestStructBasicTypes()
-        setupTestStructCustomTypes()
-    }
-    
-    // MARK: Setup utilities
-    
-    private func setupIntContainer() {
-        intContainer = [1, 5, 3, 7, 8, 8, 4, 1, 2, 1]
-    }
-    
-    private func setupStringContainer() {
-        stringContainer = ["aaa", "xfd", "aaaXXXX", "people"]
-    }
-    
-    private func setupCustomClassContainer() {
-        customClassContainer.append(CustomClass(name: "John", score: 4.65))
-        customClassContainer.append(CustomClass(name: "David", score: 4))
-        customClassContainer.append(CustomClass(name: "Alex", score: 3.1))
-        customClassContainer.append(CustomClass(name: "Karine", score: 2.2))
-    }
-    
-    private func setupTestStructBasicTypes() {
-        testStructBasicTypes.append(TestStructBasicTypes(name: "John", score: 2.56))
-        testStructBasicTypes.append(TestStructBasicTypes(name: "David", score: 4))
-        testStructBasicTypes.append(TestStructBasicTypes(name: "Alex", score: 4.2))
-        testStructBasicTypes.append(TestStructBasicTypes(name: "Karine", score: 3.35))
-        testStructBasicTypes.append(TestStructBasicTypes(name: "Alex", score: 4.2))
-    }
-    
-    private func setupTestStructCustomTypes() {
-        testStructCustomTypes.append(TestStructCustomTypes(ID: 1, object: CustomClass(name: "John", score: 4.65)))
-        testStructCustomTypes.append(TestStructCustomTypes(ID: 2, object: CustomClass(name: "David", score: 4)))
-        testStructCustomTypes.append(TestStructCustomTypes(ID: 3, object: CustomClass(name: "Alex", score: 4.2)))
-        testStructCustomTypes.append(TestStructCustomTypes(ID: 4, object: CustomClass(name: "Karine", score: 2.2)))
-    }
+    private let containers = TestContainerHelper()
     
     // MARK: Test methods
     
     func test_count() {
-        var count = SwiftAlgo.count(value: 1, inContainer: intContainer)
+        var count = SwiftAlgo.count(value: 1, inContainer: containers.intContainer)
         XCTAssertEqual(count, 3)
         
-        count = SwiftAlgo.count(value: "aaa", inContainer: stringContainer)
+        count = SwiftAlgo.count(value: "aaa", inContainer: containers.stringContainer)
         XCTAssertEqual(count, 1)
         
-        count = SwiftAlgo.count(value: CustomClass(name: "Alex", score: 5), inContainer: customClassContainer)
+        count = SwiftAlgo.count(value: CustomClass(name: "Alex", score: 5), inContainer: containers.customClassContainer)
         XCTAssertEqual(count, 0)
         
-        count = SwiftAlgo.count(value: CustomClass(name: "Alex", score: 3.1), inContainer: customClassContainer)
+        count = SwiftAlgo.count(value: CustomClass(name: "Alex", score: 3.1), inContainer: containers.customClassContainer)
         XCTAssertEqual(count, 1)
         
-        count = SwiftAlgo.count(value: TestStructBasicTypes(name: "Alex", score: 4.2), inContainer: testStructBasicTypes)
+        count = SwiftAlgo.count(value: TestStructBasicTypes(name: "Alex", score: 4.2), inContainer: containers.testStructBasicTypes)
         XCTAssertEqual(count, 2)
         
-        count = SwiftAlgo.count(value: TestStructCustomTypes(ID: 3, object: CustomClass(name: "Alex", score: 4.2)), inContainer: testStructCustomTypes)
+        count = SwiftAlgo.count(value: TestStructCustomTypes(ID: 3, object: CustomClass(name: "Alex", score: 4.2)), inContainer: containers.testStructCustomTypes)
         XCTAssertEqual(count, 1)
     }
     
     func test_countWithPredicate() {
-        var count = SwiftAlgo.count(inContainer: intContainer, withPredicate: { $0 > 4 })
+        var count = SwiftAlgo.count(inContainer: containers.intContainer, withPredicate: { $0 > 4 })
         XCTAssertEqual(count, 4)
         
-        count = SwiftAlgo.count(inContainer: stringContainer, withPredicate: { $0.contains("aaa") })
+        count = SwiftAlgo.count(inContainer: containers.stringContainer, withPredicate: { $0.contains("aaa") })
         XCTAssertEqual(count, 2)
         
-        count = SwiftAlgo.count(inContainer: customClassContainer, withPredicate: { $0.score > 3 })
+        count = SwiftAlgo.count(inContainer: containers.customClassContainer, withPredicate: { $0.score > 3 })
         XCTAssertEqual(count, 3)
         
-        count = SwiftAlgo.count(inContainer: testStructBasicTypes, withPredicate: { $0.score > 3 })
+        count = SwiftAlgo.count(inContainer: containers.testStructBasicTypes, withPredicate: { $0.score > 3 })
         XCTAssertEqual(count, 4)
         
-        count = SwiftAlgo.count(inContainer: testStructCustomTypes, withPredicate: { $0.ID > 2 })
+        count = SwiftAlgo.count(inContainer: containers.testStructCustomTypes, withPredicate: { $0.ID > 2 })
         XCTAssertEqual(count, 2)
     }
     
